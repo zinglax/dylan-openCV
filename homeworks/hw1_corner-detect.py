0e34c73b1003718c42b517187d39c5e20d59b806
zinglax <dylanzingler@gmail.com>
2014-10-05 12:40:48 -0400

    got the program to recognize both black and white squares

--- homeworks/hw1_corner-detect.py
+++ homeworks/hw1_corner-detect.py
@@ -86,7 +86,7 @@ def salt_n_pepper(img, percentage):
     return img
 
 #---------------------------------------------------
-def rectangles(img):
+def all_corners(img):
     ''' Generates the image with all the rectangels 
     in it'''
     image1 = corner(img, 1, b_l)
@@ -135,27 +135,34 @@ def threshold_image(img, inverse=False):
 def corners(image):
     '''Returns a dictionary of possible 4 corners of a rectangle'''
     image = threshold_image(image)
-    #image_inverse = threshold_image(image, True)
     
     corners = {}
     image1 = corner(image, 1, b_l)
+    #display_image(image)
+    #display_image(image1)
    # image1_i = corner(image, 1, b_l)
     corners['bottom_left'] = get_whites(threshold_image(image1))
     #corners['bottom_left'][0:0] = get_whites(threshold_image(image1_i))
     
-    image2 = corner(img, 1, b_r)
+    image2 = corner(image, 1, b_r)
+    #display_image(image2)
+    
     #image2_i = corner(image, 1, b_l)
     corners['bottom_right'] = get_whites(threshold_image(image2))
     #corners['bottom_right'][0:0] = get_whites(threshold_image(image2_i))
     
     
-    image3 = corner(img, 1, t_l)
+    image3 = corner(image, 1, t_l)
+    #display_image(image3)
+    
     #image3_i = corner(image, 1, b_l)
     corners['top_left'] = get_whites(threshold_image(image3))
     #corners['top_left'][0:0] = get_whites(threshold_image(image3_i))
     
 
-    image4 = corner(img, 1, t_r)    
+    image4 = corner(image, 1, t_r)    
+    #display_image(image4)
+    
     #image4_i = corner(image, 1, b_l)
     corners['top_right'] = get_whites(threshold_image(image4))
     #corners['top_right'][0:0] = get_whites(threshold_image(image4_i))
@@ -198,9 +205,7 @@ def find_rectangle(pt, point_dict):
     ''' finds a rectangle given a top_left point'''
     
     rectangle = {}
-    
 
-    
     rectangle["top_left"] = pt
     rectangle["bottom_left"] = poss_corner_below(pt, point_dict["bottom_left"])
     rectangle["top_right"] = poss_corner_right(pt, point_dict["top_right"])      
@@ -220,7 +225,7 @@ def find_rectangle(pt, point_dict):
     return rectangle, point_dict
     
 
-def draw_rectangle(image, rect):
+def draw_rectangle(image, rect, i):
     rect = flip_xy(rect)
     
     cv2.line(image, tuple(rect["top_left"]), tuple(rect["top_right"]), (0,255,0), 1)
@@ -229,13 +234,40 @@ def draw_rectangle(image, rect):
     cv2.line(image, tuple(rect["bottom_left"]), tuple(rect["bottom_right"]), (0,255,0), 1)
     
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(image,"hello", tuple(rect["bottom_left"]), font, .2, (0,255,0))    
+    placement = list(rect["bottom_left"])
+    placement[0] = placement[0] + 5 
+    placement[1] = placement[1] - 5
+    cv2.putText(image,str(i), tuple(placement), font, .2, (0,255,0))    
     
 def flip_xy(rect):
     for key in rect:
         rect[key] = (rect[key][1],rect[key][0])
     return rect
+
+def all_rectangles(img):
+    c = corners(img)
+    rects = []    
     
+    print c
+
+    c["top_left"].sort(key=lambda x: x[1])
+        
+    for tl in c["top_left"]:
+        r, c =  find_rectangle(tl, c)
+
+        
+        if not(r == None):
+            rects.append(r)
+
+    print len(rects)
+    return rects
+
+def draw_all_rectangles(img, rects):
+    ''' Draws all of the rectangles to the image'''
+    for i,r in enumerate(rects):
+        draw_rectangle(img, r, i)
+    display_image(img)    
+        
 if __name__== "__main__":    
     
     # images
@@ -244,128 +276,33 @@ if __name__== "__main__":
     #img = cv2.imread('../images/ovechkin-skillscomp.jpg')
     #img = cv2.imread('../images/skyscrapers.jpg')
     #img = cv2.imread('../images/checkerboard.jpg')
-    #img = cv2.imread('../images/checkerboard2.jpg')
+    img = cv2.imread('../images/checkerboard2.jpg')
     #img = cv2.imread('../images/squareb.jpg')
     #img = cv2.imread('../images/squarew.jpg') 
-    img = cv2.imread('../images/sq.jpg')  
+    #img = cv2.imread('../images/sq.jpg')  
+    #img = cv2.imread('../images/checkbb.jpg')
+    #img = cv2.imread('../images/planner.jpg')
+    #img = cv2.imread('../images/redcheck.jpg')
+    
+    
        
-    #img = salt_n_pepper(img,.03)
-    #img = blur(img, 2, 1)
+    #img = salt_n_pepper(img,3)
+    #img = blur(img, 5, 2)
+    #img = cv2.medianBlur(img,7)
+    
     #img = rectangles(img)
     
     
-    c = corners(img)
-    #img = corner(img, 1, b_r)
-    img = rectangles(img)
+    r = all_rectangles(img)
+    img_i = threshold_image(img, inverse=True)
+    
+    r[0:0] = all_rectangles(img_i)
 
-    rects = []
-    count = 0
+    img = threshold_image(img)
+    img_i = corner(img_i, 1, b_r)
+    #display_image(all_corners(img_i))
+    #display_image(img_i)
     
-    d = c
-    print c
-    
-    c["top_left"].sort(key=lambda x: x[1])
-    
-    
-    rects = []
-    for tl in c["top_left"]:
-        r, d =  find_rectangle(tl, d)
+    draw_all_rectangles(img, r)
 
-        
-        if not(r == None):
-            rects.append(r)
-
-
-    print len(rects)
-    for r in rects:
-        draw_rectangle(img, r)
-        
-        #if count == 15:
-            #display_image(img)
-            ##break
-        #count = count + 1 
-        
-    
-    print count
-
-    display_image(img)
-    
-    
-    
-   
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    #print c['top_left'][0:3]
-    #print c['bottom_left'][0:3]
-    #print c['top_right'][0:3]
-    #print c['bottom_right'][0:3]
-    
-    
-    #rects = {}
-    #for tl in c['top_left']:
-        
-        ## Possible Bottom Lefts
-        #poss_bl = []
-        #for bl in c['bottom_left']:
-            
-            ## if the bottom left col value is +- 3 away from top left
-            #if bl[1] < tl[1]+3 and bl[1] > tl[1]-3:
-                #poss_bl.append(bl)
-                
-        
-        ## Sorts the list of list on the first value of inner list
-        #poss_bl.sort(key=lambda x: x[0])
-        
-        ## Finds all of the other points possible corners in the local area
-        #minimum = poss_bl[0][0]
-        #for bl in poss_bl:
-            #if not(bl[0] >= minimum and bl[0] <= 6):
-                #poss_bl.remove(bl)
-        
-        ## Finding all of the possible bottom lefts for this rectangle
-        
-        
-        ## TODO
-        ## Need to find minimum row value and then add trim all the ones that are not +-3 from this value
-        ## This basically gets all of the possible bottom lefts directly below this top left
-        ##
-        ## Next Do this with the top Rights in the same fashion 
-        ## Finally From all of your top rights and bottom lefts find your bottom rights
-        ## 
-        ## Might want to create a function that finds all of the possible points above, below, left, or right of a given point
-        ##
-        ## Once you have all of the possible points for this rectangle, draw them...
-        
-        
-        #rects[tuple(tl)] = {'poss_bl':poss_bl}
-        
-        
-        
-        
-        ## Possible Top Rights
-    
-    
-        
-    #print rects
     
